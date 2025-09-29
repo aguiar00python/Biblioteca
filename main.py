bb463a67b905d6854936674a09f0fe51fd5bb978
kaua <bolologordinhodocaraio123@gmail.com>
2025-09-29 10:31:35 -0300

    etapa 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ conexao = sqlite3.connect("biblioteca.db")
 
 cursor = conexao.cursor()
 
-#ETAPA 1
+#ETAPA 1 - criando uma tabela
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS livros (
@@ -15,4 +15,25 @@ CREATE TABLE IF NOT EXISTS livros (
     )                
 """)
 print("Tabela criada com sucesso!")
+
+#ETAPA 2 - cadastrando livros
+
+def cadastrar_livros():
+    conexao = sqlite3.connect("biblioteca.db")
+    cursor = conexao.cursor()
+    titulo = input("Digite o título do livro: ")
+    autor = input("Digite o nome do autor do livro: ")
+    ano = input("Digite o ano do livro: ")
+    disponivel = "sim"
+    
+    cursor.execute("""
+        INSERT INTO livros (titulo, autor, ano, disponivel)
+        VALUES (?, ?, ?, ?)
+    """, (titulo, autor, ano, disponivel))
+    
+    conexao.commit()
+    conexao.close()
+
+cadastrar_livros()
+print("Livro cadastrado com sucesso!")
 
